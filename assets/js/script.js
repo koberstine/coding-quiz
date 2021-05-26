45d0a8a37d01c74baf6a7a8f9a9c3663f5375bf6
Hammurabi <koberstine@hotmail.com>
2021-05-26 12:40:37 -0400

    removed event handler from start button

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,10 +2,12 @@
 var heading = document.getElementById("heading");
 var content = document.getElementById("content");
 var buttonArea = document.getElementById("buttonArea");
+var button = document.createElement("button");
 var counter = 0;
+var timer = 180;
 var question = [ 
     {
-        "questionText" : "This is a blank question",
+        "questionText" : "This question intentionally left blank",
         "correctAnswer" : 1,
         "answerList" : [
             "answer 1"
@@ -15,75 +17,81 @@ var question = [
         "questionText" : "This is question 1",
         "correctAnswer" : 1,
         "answerList" : [
-            "answer 1",
-            "answer 2",
-            "answer 3",
-            "answer 4"
+            "q1 answer 1",
+            "q1 answer 2",
+            "q1 answer 3",
+            "q1 answer 4"
         ]
     },
     {   
         "questionText" : "This is question 2",
         "correctAnswer" : 1,
         "answerList" : [
-            "answer 1",
-            "answer 2"
+            "q2 answer 1",
+            "q2 answer 2"
         ]
     },
     {
         "questionText" : "This is question 3",
         "correctAnswer" : 1,
         "answerList" : [
-            "answer 1",
-            "answer 2",
-            "answer 3",
-            "answer 4"
+            "q3 answer 1",
+            "q3 answer 2",
+            "q3 answer 3",
+            "q3 answer 4"
         ]
     }
 ];
 
 function offerQuiz () {
     heading.innerText = "Coding Quiz Challenge";
-    document.body.appendChild(heading); 
 
     content.innerText = "Try to answer the following code related questions within the time limit. Answer carefully because incorrect answers will decrease your remaining time by 10 seconds.";
-    document.body.appendChild(content);
 
-    var button = document.createElement("button");
     button.textContent = "Start";
     buttonArea.appendChild(button);
-    button.addEventListener("click", quizMain)
+    button.onClick = quizMain();
 };
 
 function quizMain () {
-    
-   do  {
-    counter = counter +1;
-    //remove previous buttons
-    document.body.removeChild(buttonArea);
+    questionPrep();
+
+};
+
+function questionPrep() {
+    if (counter < question.length) {
+        counter++;
+        var buttons = buttonArea.getElementsByTagName('button');
+        if (buttons) {
+            for (var i = 0; i < buttons.length; i++) {
+                buttons[i].remove();
+            }
+        };
+
+    renderQuestion ();
+    } else {
+        console.log("end");
+    };
+};
+
+function renderQuestion () {
 
     heading.innerText = "Question " + counter;
-    document.body.appendChild(heading); 
-
     content.innerText = question[counter].questionText;
-    document.body.appendChild(content);
-    document.body.appendChild(buttonArea);
-
-    for ( i =0; i < question[counter].answerList.length; i++) {
-        var button = document.createElement("button");
-        button.textContent = question[counter].answerList[i];
+    for (var j=0; j<question[counter].answerList.length; j++) {
+        button.textContent = question[counter].answerList[j];
+        button.setAttribute("id", j);
         buttonArea.appendChild(button);
-        button.addEventListener("click", checkAnswer)
+        button.addEventListener("click", function() {checkAnswer(j)});
+        };
     }
+    
 
-    } while (counter <= question.length);
 
-    console.log("finished");
-};
-
-function checkAnswer () {
-    console.log("click")
-    return;
-};
+function checkAnswer(response) {
+    console.log(response + " was clicked");
+    questionPrep();
+}
 
 offerQuiz ();
 
